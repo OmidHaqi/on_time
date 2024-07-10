bfc8ea379e0fc1f4264317752f3783f3e44ee4d2
Umut <omid.haqi@outlook.com>
2024-07-10 20:02:27 +0330

    add some dependency and ceate pesian calender

---
+++ lib/screens/home/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:on_time/resource/utils/presian_calendar.dart';
+import 'package:persian_datetime_picker/persian_datetime_picker.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: Center(
+        child: Directionality(
+          textDirection: TextDirection.rtl,
+          child: PersianCalendar(
+            firstDate: Jalali(1385, 8),
+            initialDate: Jalali.now(),
+            lastDate: Jalali(1450, 8),
+            onDateChanged: (Jalali? value) {
+              //TODO:Dont forget
+            },
+          ),
+        ),
+      ),
+    ));
+  }
+}
