1ad3cc6e18eb949158454446312f2bf88c0241ba
Umut <omid.haqi@outlook.com>
2024-07-10 20:02:46 +0330

    add light and dark theme

---
+++ lib/resource/themes.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  
+ static ThemeData darkTheme() {
+    return ThemeData(
+      fontFamily: 'YekanBakh',
+      colorScheme: const ColorScheme(
+          brightness: Brightness.dark,
+          primary: Color(0xffDAE5EB),
+          onPrimary: Color(0xff161928),
+          secondary: Color(0xff161928),
+          onSecondary: Color(0xffDAE5EB),
+          error: Color(0xffFF636C),
+          onError: Color(0xffDAE5EB),
+          surface: Color(0xff161928),
+          onSurface: Color(0xffDAE5EB)),
+    );
+  }
+
+  static ThemeData lightTheme() {
+    return ThemeData(
+      fontFamily: 'YekanBakh',
+      colorScheme: const ColorScheme(
+          brightness: Brightness.light,
+          primary: Color(0xff161928),
+          onPrimary: Color(0xffDAE5EB),
+          secondary: Color(0xffDAE5EB),
+          onSecondary: Color(0xff161928),
+          error: Color(0xffFF636C),
+          onError: Color(0xffDAE5EB),
+          surface: Color(0xffDAE5EB),
+          onSurface: Color(0xff161928)),
+    );
+  }
+}
