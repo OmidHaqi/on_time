c9c9dc4fd68a9caa54449db0413ed50194840954
Umut <omid.haqi@outlook.com>
2024-07-23 13:32:53 +0330

    add flutter_staggered_animations

--- lib/index.dart
+++ lib/index.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:on_time/data/models/language_model.dart';
