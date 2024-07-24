7489105a1108c9130fbf08e6a2e03e9f0965beaf
Umut <omid.haqi@outlook.com>
2024-07-24 12:32:25 +0330

    refactor homeBloC to taskBloC

---
+++ lib/screens/home/task/bloc/task_event.dart
@@ -0,0 +1,45 @@
+part of 'task_bloc.dart';
+
+sealed class TaskEvent extends Equatable {
+  const TaskEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class TaskInit extends TaskEvent {}
+
+class TaskLoadedEvent extends TaskEvent {
+  final List<TaskModel> allTaskList;
+  const TaskLoadedEvent(this.allTaskList);
+  @override
+  List<Object> get props => [allTaskList];
+}
+
+class DeleteTaskEvent extends TaskEvent {
+  final int id;
+  const DeleteTaskEvent(this.id);
+
+  @override
+  List<Object> get props => [id];
+}
+
+class SaveTaskEvent extends TaskEvent {
+  final TaskModel saveTask;
+  const SaveTaskEvent(this.saveTask);
+
+  @override
+  List<Object> get props => [saveTask];
+}
+
+class UpdateTaskEvent extends TaskEvent {
+  final int id;
+  final TaskModel updateTask;
+
+  const UpdateTaskEvent(this.updateTask, this.id);
+
+  @override
+  List<Object> get props => [updateTask, id];
+}
+
+final class DeleteAllTasksEvent extends TaskEvent {}
---
+++ lib/screens/home/task/bloc/task_state.dart
@@ -0,0 +1,51 @@
+part of 'task_bloc.dart';
+
+sealed class TaskState extends Equatable {
+  const TaskState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class TaskLoadingState extends TaskState {}
+
+final class TaskLoadedState extends TaskState {
+  final List<TaskModel> taskList;
+  const TaskLoadedState(this.taskList);
+  @override
+  List<Object> get props => [taskList];
+}
+
+final class DeleteTaskState extends TaskState {
+  final List<TaskModel> taskTask;
+  const DeleteTaskState(this.taskTask);
+
+  @override
+  List<Object> get props => [taskTask];
+}
+
+final class SaveTaskState extends TaskState {
+  final List<TaskModel> taskList;
+  const SaveTaskState(this.taskList);
+
+  @override
+  List<Object> get props => [taskList];
+}
+
+final class UpdateTaskState extends TaskState {
+  final List<TaskModel> taskList;
+  const UpdateTaskState(this.taskList);
+
+  @override
+  List<Object> get props => [taskList];
+}
+
+final class DeleteAllTasksState extends TaskState {
+  final List<TaskModel> taskList;
+  const DeleteAllTasksState(this.taskList);
+
+  @override
+  List<Object> get props => [taskList];
+}
+
+final class TaskError extends TaskState {}
