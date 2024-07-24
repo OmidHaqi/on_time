68651dea6a8a26ec5060a1ebb2ebb0b1290001d8
Umut <omid.haqi@outlook.com>
2024-07-24 18:21:06 +0330

    add fab for add note btn

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -16,7 +16,7 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void dispose() {
-    TaskLocalDataSrc taskLocalDataSrc = TaskLocalDataSrc();
+    LocalDataSrc taskLocalDataSrc = LocalDataSrc();
     taskLocalDataSrc.close();
     super.dispose();
   }
@@ -27,98 +27,107 @@ class _HomePageState extends State<HomePage> {
     int selectedScreen = selectedPageIndex;
     return SafeArea(
       child: Scaffold(
-        appBar: const CustomAppBar(
-          appBarHeight: 100,
-          child: Text(
-            AppStrings.appName,
-            style: AppTextStyles.appNameTextStyle,
+          appBar: const CustomAppBar(
+            appBarHeight: 100,
+            child: Text(
+              AppStrings.appName,
+              style: AppTextStyles.appNameTextStyle,
+            ),
           ),
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: size.width / 1.38,
-                  height: size.height / 16.4,
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.primary,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      ZoomTapAnimation(
-                        onTap: () {
-                          setState(() {
-                            changeScreen(0);
-                          });
-                        },
-                        child: Container(
-                          width: size.width / 2.9,
-                          height: size.height / 19,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                            color: selectedScreen == 0
-                                ? Theme.of(context).colorScheme.onPrimary
-                                : Colors.transparent,
-                          ),
-                          child: Center(
-                            child: Text(AppStrings.planning,
-                                style: AppTextStyles.chipTextStyle.apply(
-                                  color: selectedScreen == 0
-                                      ? Theme.of(context).colorScheme.primary
-                                      : Theme.of(context).colorScheme.onPrimary,
-                                )),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: size.width / 1.38,
+                    height: size.height / 16.4,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primary,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        ZoomTapAnimation(
+                          onTap: () {
+                            setState(() {
+                              changeScreen(0);
+                            });
+                          },
+                          child: Container(
+                            width: size.width / 2.9,
+                            height: size.height / 19,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: selectedScreen == 0
+                                  ? Theme.of(context).colorScheme.onPrimary
+                                  : Colors.transparent,
+                            ),
+                            child: Center(
+                              child: Text(AppStrings.planning,
+                                  style: AppTextStyles.chipTextStyle.apply(
+                                    color: selectedScreen == 0
+                                        ? Theme.of(context).colorScheme.primary
+                                        : Theme.of(context)
+                                            .colorScheme
+                                            .onPrimary,
+                                  )),
+                            ),
                           ),
                         ),
-                      ),
-                      ZoomTapAnimation(
-                        onTap: () {
-                          setState(() {
-                            changeScreen(1);
-                          });
-                        },
-                        child: Container(
-                          width: size.width / 2.9,
-                          height: size.height / 19,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                            color: selectedScreen == 1
-                                ? Theme.of(context).colorScheme.onPrimary
-                                : Colors.transparent,
-                          ),
-                          child: Center(
-                            child: Text(
-                              AppStrings.note,
-                              style: AppTextStyles.chipTextStyle.apply(
-                                color: selectedScreen == 1
-                                    ? Theme.of(context).colorScheme.primary
-                                    : Theme.of(context).colorScheme.onPrimary,
+                        ZoomTapAnimation(
+                          onTap: () {
+                            setState(() {
+                              changeScreen(1);
+                            });
+                          },
+                          child: Container(
+                            width: size.width / 2.9,
+                            height: size.height / 19,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: selectedScreen == 1
+                                  ? Theme.of(context).colorScheme.onPrimary
+                                  : Colors.transparent,
+                            ),
+                            child: Center(
+                              child: Text(
+                                AppStrings.note,
+                                style: AppTextStyles.chipTextStyle.apply(
+                                  color: selectedScreen == 1
+                                      ? Theme.of(context).colorScheme.primary
+                                      : Theme.of(context).colorScheme.onPrimary,
+                                ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-            IndexedStack(
-              alignment: Alignment.center,
-              index: selectedPageIndex,
-              children: const [
-                Tasks(),
-                Notes(),
-              ],
-            ),
-          ],
-        ),
-      ),
+                ],
+              ),
+              IndexedStack(
+                alignment: Alignment.center,
+                index: selectedPageIndex,
+                children: const [
+                  Tasks(),
+                  Notes(),
+                ],
+              ),
+            ],
+          ),
+          floatingActionButton: selectedPageIndex == 1
+              ? FloatingActionButton(
+                  child: Icon(
+                    Icons.add,
+                    color: Theme.of(context).colorScheme.surface,
+                  ),
+                  onPressed: () {})
+              : SizedBox.shrink()),
     );
   }
 }
