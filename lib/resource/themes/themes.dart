557aabb9556e8dcd4e12cb7b9c827901a77e98ad
Umut <omid.haqi@outlook.com>
2024-07-30 18:23:09 +0330

    add dialog theme

--- lib/resource/themes/themes.dart
+++ lib/resource/themes/themes.dart
@@ -5,15 +5,24 @@ class AppTheme {
     return ThemeData(
       fontFamily: 'YekanBakh',
       colorScheme: const ColorScheme(
-          brightness: Brightness.dark,
-          primary: Color(0xffDAE5EB),
-          onPrimary: Color(0xff161928),
-          secondary: Color(0xff161928),
-          onSecondary: Color(0xffDAE5EB),
-          error: Color(0xffFF636C),
-          onError: Color(0xffDAE5EB),
-          surface: Color(0xff161928),
-          onSurface: Color(0xffDAE5EB)),
+        brightness: Brightness.dark,
+        primary: Color(0xffDAE5EB),
+        onPrimary: Color(0xff161928),
+        secondary: Color(0xff161928),
+        onSecondary: Color(0xffDAE5EB),
+        error: Color(0xffFF636C),
+        onError: Color(0xffDAE5EB),
+        surface: Color(0xff161928),
+        onSurface: Color(0xffDAE5EB),
+      ),
+      dialogTheme: DialogTheme(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+          side: const BorderSide(
+            color: Color(0xffDAE5EB),
+          ),
+        ),
+      ),
     );
   }
 
@@ -21,15 +30,24 @@ class AppTheme {
     return ThemeData(
       fontFamily: 'YekanBakh',
       colorScheme: const ColorScheme(
-          brightness: Brightness.light,
-          primary: Color(0xff161928),
-          onPrimary: Color(0xffDAE5EB),
-          secondary: Color(0xffDAE5EB),
-          onSecondary: Color(0xff161928),
-          error: Color(0xffFF636C),
-          onError: Color(0xffDAE5EB),
-          surface: Color(0xffDAE5EB),
-          onSurface: Color(0xff161928)),
+        brightness: Brightness.light,
+        primary: Color(0xff161928),
+        onPrimary: Color(0xffDAE5EB),
+        secondary: Color(0xffDAE5EB),
+        onSecondary: Color(0xff161928),
+        error: Color(0xffFF636C),
+        onError: Color(0xffDAE5EB),
+        surface: Color(0xffDAE5EB),
+        onSurface: Color(0xff161928),
+      ),
+      
+      dialogTheme:  DialogTheme(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+            side: const BorderSide(
+              color: Color(0xff161928),
+            )),
+      ),
     );
   }
 }
