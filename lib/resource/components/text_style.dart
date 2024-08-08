b6bc7c9671ff65cd4353924fca25797ece7a7e62
Umut <omid.haqi@outlook.com>
2024-08-08 12:50:50 +0330

    add new TextStyle

--- lib/resource/components/text_style.dart
+++ lib/resource/components/text_style.dart
@@ -20,12 +20,10 @@ class AppTextStyles {
     fontWeight: FontWeight.bold,
   );
   static const TextStyle taskInfoTitleTextStyle = TextStyle(
-
     fontSize: 18,
     fontWeight: FontWeight.bold,
   );
   static const TextStyle taskInfoTextStyle = TextStyle(
-
     fontSize: 16,
   );
   static const TextStyle taskDateTimeTextStyle = TextStyle(
@@ -33,60 +31,51 @@ class AppTextStyles {
     fontWeight: FontWeight.bold,
   );
   static const TextStyle appBarTitle = TextStyle(
-
     fontSize: 24,
     fontWeight: FontWeight.bold,
   );
   static const TextStyle dateTimeTextStyle = TextStyle(
-
     fontSize: 24,
   );
 
   static const TextStyle appTopText = TextStyle(
-
     fontSize: 26,
     fontWeight: FontWeight.bold,
   );
 
   static const TextStyle emptyTextStyle = TextStyle(
-
     fontSize: 18,
   );
 
   static const TextStyle noteTitleTextStyle = TextStyle(
-
     fontSize: 24,
     fontWeight: FontWeight.bold,
   );
   static const TextStyle introScreenBtnTextStyle = TextStyle(
-
     fontSize: 22,
     fontWeight: FontWeight.bold,
   );
   static const TextStyle noteDecTextStyle = TextStyle(
-
     fontSize: 20,
   );
   static const TextStyle noteDateTimeTextStyle = TextStyle(
-
     fontSize: 18,
   );
   static const TextStyle splashScreenTextStyle = TextStyle(
-
     fontWeight: FontWeight.w800,
     fontSize: 45,
   );
   static const TextStyle introScreenDesTextStyle = TextStyle(
-
     fontWeight: FontWeight.w400,
     fontSize: 22,
   );
   static const TextStyle isComplatedTextStyle = TextStyle(
-
     fontWeight: FontWeight.w700,
   );
   static const TextStyle selectedTextStyle = TextStyle(
-
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle appInfoTextStyle = TextStyle(
     fontWeight: FontWeight.bold,
   );
 }
